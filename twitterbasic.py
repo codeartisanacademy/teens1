eb74548b30339b498b9332e6f413bd39b7fa6ad0
wahyudi <wahyudi.coding@gmail.com>
2019-08-08 09:09:08 +0700

    add tweepy

--- twitterbasic.py
+++ twitterbasic.py
@@ -1,15 +1,17 @@
 import tweepy as tw
 
+consumer_key = '3AJlDF2sOLiacWfTwB4kfQ'
+consumer_secret = 'N0BoMJRjiixJo7IHdqupdqx7gYCksgRsAGF9gHWYTI'
+access_token = '14128035-vJOHnsTqjD8gOcyysOAvDHCOiLMZ8ZyKL1v6ZguXQ'
+access_token_secret = 'XkEEdb2MH2OJVGYQtiV7eyXZrGpHzsuz7dRQek1j4'
 
-x = 0
-while x < 6:
-    print(x)
-    x += 1
+# login or authenticate to twitter
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
+api = tw.API(auth, wait_on_rate_limit=True)
 
-
-
-
+# api.update_status('hello world from cipete')
 
 
 
