bb94515ef05b1ed6ad8505dc1eca618192fe9edd
wahyudi <wahyudi.coding@gmail.com>
2019-08-08 09:32:06 +0700

    search for a keyword

--- twitterbasic.py
+++ twitterbasic.py
@@ -13,8 +13,14 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 # api.update_status('hello world from cipete')
 
+search_words = "#quotes"
+date_since = "2019-08-01"
 
+tweets = tw.Cursor(api.search, q=search_words, lang="en", since=date_since).items(1)
 
+for t in tweets:
+    print(t.text + ' at ' + t.user.location)
+    print(t.user.description)
 
 
 
